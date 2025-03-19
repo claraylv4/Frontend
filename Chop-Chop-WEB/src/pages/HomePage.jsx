efc43391689a41e7fdf3432016682796fc601dbe
claraylv4 <163171966+claraylv4@users.noreply.github.com>
2025-03-19 13:59:15 +0100

    Update HomePage.jsx

--- Chop-Chop-WEB/src/pages/HomePage.jsx
+++ Chop-Chop-WEB/src/pages/HomePage.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 
 function HomePage() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const DropDown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   
   const handleShowLogin = () => {
     window.location.href = '/login';
+  };
+
+  const handleShowLogOut = () => {
+    window.location.href = '/';
   };
 
   const handleShowSignup = () => {
     window.location.href = '/signup';
   };
 
+
   return (
     <>
       <header className='header-style'>
-        <input style={{ color: '#000000', position: 'fixed', top: '17px', left: '10px' }} placeholder='Search'/>
+        <input style={{ color: '#000000', position: 'fixed', top: '17px', left: '10px' }} placeholder='Search'/> 
         <button onClick={handleShowLogin} style={{ position: 'fixed', top: '17px', right: '10px' }}>Login</button>
         <button onClick={handleShowSignup} style={{ position: 'fixed', top: '17px', right: '60px' }}>Sign Up</button>
       </header>
 
-      <h1>Chop-Chop</h1>
+      {/* Faig que "Chop-Chop" sigui un botó per fer clic */}
+      <h1 onClick={DropDown} style={{ cursor: 'pointer' }}>Chop-Chop</h1>
+
+      {/* Mostra el desplegable només si `isDropdownOpen` és true */}
+      {isDropdownOpen && (
+        <div style={{ border: '1px solid black', padding: '10px', position: 'absolute', background: 'white' }}>
+          <a href="#" onClick={handleShowLogin} style={{ color: '#000', display: 'block', padding: '5px' }}>Log In</a>
+          <br />
+          <a href="#" onClick={handleShowSignup} style={{ color: '#000', display: 'block', padding: '5px' }}>Sign Up</a>
+          <br /> 
+          <a href="#" onClick={handleShowLogOut} style={{color: '#000', display: 'block', padding: '5px' }}>Log Out</a>
+        </div>
+      )}
+
+
     </>
-  )
+  );
 }
 
 export default HomePage
